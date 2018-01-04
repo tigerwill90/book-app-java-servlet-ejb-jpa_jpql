b0d7004e42d144838c9aa4592573f3cbdbf4ab85
Sylvain <sylvain.muller90@gmail.com>
2018-01-04 16:30:36 +0100

    Initial commit

---
+++ src/ch/sylvainmuller/servlets/DashboardServlet.java
@@ -0,0 +1,15 @@
+package ch.sylvainmuller.servlets;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+public class DashboardServlet extends HttpServlet{
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        request.getRequestDispatcher("dashboard.jsp").forward(request, response);
+    }
+}
